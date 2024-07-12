f6ae87ce6a88bcf77dee69d2070473046b3fc03e
Kerberoge <sjoerdenjh@gmail.com>
2024-07-12 13:01:53 +0200

    Modified config file to match Dell setup

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 static int top              = 1;
 static int case_insensitive = 1;
 static int item_padding     = 20;
-static const char *fonts[]  = { "DejaVu Sans Mono:size=10.5" };
+static const char *fonts[]  = { "Open Sans SemiBold:size=9" };
 static const char *prompt   = NULL;
 static uint32_t colors[][2] = {
 	/*               fg          bg          */
-	[SchemeNorm] = { 0xddddddff, 0x222033ff },
-	[SchemeSel]  = { 0x9186dbff, 0x222033ff },
+	[SchemeNorm] = { 0xeeeeeeff, 0x1d293dff },
+	[SchemeSel]  = { 0x7da5ffff, 0x1d293dff },
 };
 
 /* If nonzero, use vertical list with given number of lines */
