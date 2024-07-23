0bc34c78210b96e6197fa7b3b7c9fa6c6181da52
Kerberoge <sjoerdenjh@gmail.com>
2024-07-23 19:41:02 +0200

    Changed colors to match the Void wallpaper

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const char *fonts[]  = { "DejaVu Sans Mono:size=10.5" };
 static const char *prompt   = NULL;
 static uint32_t colors[][2] = {
 	/*               fg          bg          */
-	[SchemeNorm] = { 0xddddddff, 0x222033ff },
-	[SchemeSel]  = { 0x9186dbff, 0x222033ff },
+	[SchemeNorm] = { 0xddddddff, 0x1c1e1eff },
+	[SchemeSel]  = { 0x65ad89ff, 0x1c1e1eff },
 };
 
 /* If nonzero, use vertical list with given number of lines */
